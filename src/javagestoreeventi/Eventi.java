28eeb405a4b3bfb5e2f5e38d4d70d13b83007a28
DanielemarchioP <danielemarchio709@gmail.com>
2024-05-25 01:50:54 +0200

    add : step 2

---
+++ src/javagestoreeventi/Eventi.java
@@ -0,0 +1,79 @@
+package javagestoreeventi;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class Eventi {
+	
+	private String titolo;
+	private LocalDate data;
+	private int postiTotali;
+	private int postiPrenotati;
+	
+	public Eventi( String titolo, LocalDate data, int postiTotali) {
+		if(postiTotali > 0 && data.isAfter(LocalDate.now())) {
+			
+			this.titolo = titolo;
+			this.data = data;
+			this.postiTotali = postiTotali;
+			this.postiPrenotati = 0;
+			
+		} else {
+			System.out.print("Errore nella selezione del giorno o posti non disponibili");
+		}
+		
+	}
+	
+	public void prenota(int numeroPrenotazioni) {
+		if(data.isAfter(LocalDate.now()) && postiTotali > (postiPrenotati + numeroPrenotazioni)) {
+			postiPrenotati = postiPrenotati + numeroPrenotazioni;
+			
+			System.out.println(" i posti prenotati sono " + postiPrenotati + "i posti totali per l'evento sono "+ postiTotali + "hai effettuato " + numeroPrenotazioni + " prenotazioni");
+			
+		}else {
+			System.out.print("non e possibile prenotare per l'evento scelto");
+		}
+	}
+	public void disdici() {
+		if(data.isAfter(LocalDate.now()) && postiPrenotati != 0 ) {
+			postiPrenotati --;
+			
+		}else {
+			System.out.print("non e possibile disdire per l'evento scelto");
+		}
+	}
+
+	@Override
+	public String toString() {
+		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+		
+		return dtf.format(data) + titolo ;
+	}
+
+	public String getTitolo() {
+		return titolo;
+	}
+
+	public void setTitolo(String titolo) {
+		this.titolo = titolo;
+	}
+
+	public LocalDate getData() {
+		return data;
+	}
+
+	public void setData(LocalDate data) {
+		this.data = data;
+	}
+
+	public int getPostiTotali() {
+		return postiTotali;
+	}
+
+	public int getPostiPrenotati() {
+		return postiPrenotati;
+	}
+	
+	
+}
+
---
+++ src/javagestoreeventi/Main.java
@@ -0,0 +1,33 @@
+package javagestoreeventi;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		
+		Scanner s = new Scanner(System.in);
+		System.out.print("inserisci titolo dell'evento ");
+		String titolo = s.nextLine(); 
+		System.out.print("inserisci data dell'evento [YYYY-MM-GG] ");
+		String d = s.nextLine(); 
+		LocalDate dt = LocalDate.parse(d);
+		System.out.print("inserisci posti totali dell'evento ");
+		int postiTotali = s.nextInt();
+		
+		System.out.print(titolo + dt.toString() + postiTotali);
+		
+		
+		Eventi apex = new Eventi(titolo,dt,postiTotali);
+		
+		System.out.print("Quante prenotazioni vuoi effettuare?");
+		int numeroPrenotazioni = s.nextInt();
+		
+		apex.prenota(numeroPrenotazioni);
+
+	}
+
+}
